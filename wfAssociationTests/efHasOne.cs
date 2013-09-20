1adf5c0a072dea0884722bcae2d02c4926c5bfa8
David Thole <dthole@gmail.com>
2013-09-20 11:15:50 -0500

    Initial Adding of Has-One

---
+++ wfAssociationTests/efHasOne.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfAssociationTests
+{
+
+    //-----------------------------------  HAS ONE --------------------------------------------------------------
+    public class efHasOne
+    {
+        public int Id { get; set; }
+        virtual public efHadOne efHad { get; set; }
+    }
+
+    public class efHadOne
+    {
+        public int Id { get; set; }
+        public String Label { get; set; }
+    }
+
+
+    //-----------------------------------  HAS MANY ---------------------------------------------------------------
+
+
+    //-----------------------------------  MANY MANY --------------------------------------------------------------
+}
